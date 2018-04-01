fdcd026a0b49025e7a9ded1d6691ec916c17bc32
praneymadan171@gmail.com <praneymadan171@gmail.com>
2018-04-01 23:52:20 +0530

    added name column in outstock

--- Modules/DbController/triggeroutStock.py
+++ Modules/DbController/triggeroutStock.py
@@ -5,14 +5,16 @@ import requests
 # Open database connection
 
 
-def triggeroutStock(sno,itemid,outprice,date):
+def triggeroutStock(name,sno,itemid,outprice,givenprice,date):
     global Serial
     global ItemId
     global InPrice
     global created_at
+    name=name
     Serial = sno
     ItemId = itemid
     outPrice = outprice
+    givenprice = givenprice
     sold_at = date
 
 
@@ -24,9 +26,9 @@ def triggeroutStock(sno,itemid,outprice,date):
 
     try :
         with connection.cursor() as cursor:
-            sql = "INSERT into outstock (sno,item_id,out_price,sold_at) values (%s,%s, %s,%s)"
+            sql = "INSERT into outstock (sno,item_id,out_price,sold_at,given_price,name) values (%s,%s, %s,%s,%s,%s)"
             sql1="update instock set status=0 where sno="+Serial
-            cursor.execute(sql, (Serial,ItemId,outPrice,sold_at))
+            cursor.execute(sql, (Serial,ItemId,outPrice,sold_at,givenprice,name))
             cursor.execute(sql1)
             # test_func()
 
--- Modules/Services/setOutStock.py
+++ Modules/Services/setOutStock.py
@@ -4,7 +4,6 @@ import time
 from flask import Flask, json, jsonify, render_template, session, request, redirect, url_for, abort, session, flash
 from flask_restful import Resource, Api
 from flask import json
-# from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField
 from flask_wtf import Form
 from wtforms import *
 import logging
@@ -16,15 +15,14 @@ import subprocess
 from celery import Celery
 from DbController import createEntryStock
 from DbController import triggeroutStock
-# from wtforms_components
-# from flask.ext.admin.form.widgets import DatePickerWidget
-# from flask.ext.admin.form.widgets import DatePickerWidget
 
 
 class ReusableForm(Form):
-    sno =IntegerField('Serial No', validators=[validators.required(),validators.input_required()])
+    name =StringField('Name', validators=[validators.required(),validators.input_required()])
+    sno =IntegerField('Tyre No', validators=[validators.required(),validators.input_required()])
     itemid =IntegerField('Item ID', validators=[validators.required(),validators.input_required()])
     outprice =IntegerField('Out Price', validators=[validators.required(),validators.input_required()])
+    givenprice =IntegerField('Given Price', validators=[validators.required(),validators.input_required()])
     date = DateField(u'Date ', )
     Submit = SubmitField()
 
@@ -33,13 +31,15 @@ def setOutStock():
     form = ReusableForm(request.form)
     print (form.errors)
     if request.method == 'POST':
+        name=request.form['name']
         sno=request.form['sno']
         itemid=request.form['itemid']
         outprice=request.form['outprice']
+        givenprice=request.form['givenprice']
         date=request.form['date']
 
-        if form.validate():
-            triggeroutStock.triggeroutStock(sno,itemid,outprice,date)
+        if form.validate(): 
+            triggeroutStock.triggeroutStock(name,sno,itemid,outprice,givenprice,date)
             print("success")
         else:
             flash('Error: All the form fields are required. ')
