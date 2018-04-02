d7d0e095555e46e9c5cd32bfe016efc0c61317d8
praneymadan171@gmail.com <praneymadan171@gmail.com>
2018-04-02 12:09:46 +0530

    tyre no changed from integer to string

--- Modules/Services/setInStock.py
+++ Modules/Services/setInStock.py
@@ -22,7 +22,7 @@ from DbController import trigeerinStock
 
 
 class ReusableForm(Form):
-    sno =IntegerField('Tyre No', validators=[validators.required(),validators.input_required()])
+    sno =StringField('Tyre No', validators=[validators.required(),validators.input_required()])
     itemid =IntegerField('Item ID', validators=[validators.required(),validators.input_required()])
     inprice =IntegerField('In Price', validators=[validators.required(),validators.input_required()])
     date = DateField(u'Date ', )
